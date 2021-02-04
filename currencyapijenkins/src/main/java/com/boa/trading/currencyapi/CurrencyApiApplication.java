5abf2d22766d14dcf714a87e6092e1a0380c37bc
alekhya.k85@gmail.com <rajag@host.docker.internal>
2021-02-04 16:26:58 -0500

    updated

--- currencyapijenkins/src/main/java/com/boa/trading/currencyapi/CurrencyApiApplication.java
+++ currencyapijenkins/src/main/java/com/boa/trading/currencyapi/CurrencyApiApplication.java
@@ -11,7 +11,6 @@ public class CurrencyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyApiApplication.class, args);
-	}
-	//Just test github and Jenkins
+	}	
 
 }
